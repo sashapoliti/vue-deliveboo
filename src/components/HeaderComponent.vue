ed049dbf8bf190bf47a3fde2483e32eac95e6ffc
sashapoliti <sas.politi@gmail.com>
2024-07-01 14:54:45 +0200

    add palette colors variables, some style in navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,8 @@
 <template>
-    <header>
-        Header
+    <header class="d-flex justify-content-center">
+        <nav>
+            nav
+        </nav>
     </header>
 </template>
 
@@ -11,5 +13,19 @@
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/styles/partials/_variables.scss';
 
+    header {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 80px;
+        nav {
+            width: 80%;
+            background-color: $text-color;
+            color: $primary-color;
+            border-bottom-left-radius: 20px;
+            border-bottom-right-radius: 20px;
+        }
+    }
 </style>
