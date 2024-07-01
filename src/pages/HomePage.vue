bda03b4e79a1e2043b76ad78fc5723d1a38af629
sashapoliti <sas.politi@gmail.com>
2024-07-01 23:46:20 +0200

    add city imgs for jumbo, add transition

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,18 +1,27 @@
 <template>
+  <!-- <div class="foods"></div> -->
   <div id="jumbotron">
-    <div class="city">
-      <img src="/images/jumbotron/city1.png" alt="Immagine della città" />
-    </div>
+    <transition name="slide" mode="out-in">
+      <div class="city" :key="currentCityIndex">
+        <img :src="currentCityImage" alt="Immagine della città" />
+      </div>
+    </transition>
     <div class="rider">
       <img src="/images/jumbotron/rider.png" alt="Immagine del rider" />
     </div>
     <div class="text">
-        <h1 class="text-uppercase">
-            Morsi <br>
-            pieni <br>
-            di gusto
-        </h1>
-        <p>Benvenuto su <span class="fst-italic">Munchi</span> – il tuo servizio di food delivery che porta morsi di felicità direttamente a casa tua.</p>
+      <h1 class="text-uppercase">
+        Il tuo <br />
+        morso <br />
+        preferito
+      </h1>
+      <p>
+        Benvenuto su <span class="fst-italic">Munchi</span> – il tuo servizio di
+        food delivery che porta morsi di felicità direttamente a casa tua.
+      </p>
+      <div class="mt-5 text-center">
+        <button class="text-uppercase">vai ai ristoranti</button>
+      </div>
     </div>
   </div>
   <div class="prova2">
@@ -35,15 +44,56 @@
 <script>
 export default {
   name: "HomePage",
+  data() {
+    return {
+      cityImages: [
+        "/images/jumbotron/city1.png",
+        "/images/jumbotron/city2.png",
+        "/images/jumbotron/city3.png",
+        "/images/jumbotron/city4.png",
+      ],
+      currentCityIndex: 0,
+      intervalId: null,
+    };
+  },
+  computed: {
+    currentCityImage() {
+      return this.cityImages[this.currentCityIndex];
+    },
+  },
+  mounted() {
+    this.startImageRotation();
+  },
+  beforeDestroy() {
+    this.stopImageRotation();
+  },
+  methods: {
+    startImageRotation() {
+      this.intervalId = setInterval(() => {
+        this.currentCityIndex =
+          (this.currentCityIndex + 1) % this.cityImages.length;
+      }, 7000);
+    },
+    stopImageRotation() {
+      clearInterval(this.intervalId);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/styles/partials/_variables.scss";
 
+.foods {
+  position: absolute;
+  z-index: 4;
+  width: 100%;
+  height: 100%;
+  background-image: url("/images/jumbotron/foods.png");
+}
 #jumbotron {
   position: relative;
-  z-index: 1;
+  z-index: 5;
   width: 100%;
   height: 100%;
   padding: 80px;
@@ -51,6 +101,7 @@ export default {
   background-size: cover;
   background-position: bottom;
   background-repeat: no-repeat;
+
   .text {
     width: 600px;
     padding-top: 8%;
@@ -59,8 +110,7 @@ export default {
     h1 {
       font-size: 7rem;
       font-weight: 800;
-      letter-spacing: 7px;
-      line-height: 0.9;
+      line-height: 1.07;
       margin-left: -5px;
     }
     p {
@@ -68,20 +118,48 @@ export default {
       font-size: 1rem;
       font-weight: 500;
     }
+    button {
+      font-weight: 500;
+      outline: none;
+      color: $secondary-color;
+      padding: 1em;
+      padding-left: 2em;
+      padding-right: 2em;
+      border: 2px dashed $secondary-color;
+      border-radius: 15px;
+      background-color: $primary-color;
+      box-shadow: 0 0 0 4px $primary-color, 2px 2px 4px 2px rgba(0, 0, 0, 0.5);
+      transition: 0.1s ease-in-out, 0.4s color;
+    }
+
+    button:active {
+      transform: translateX(0.1em) translateY(0.1em);
+      box-shadow: 0 0 0 4px $primary-color,
+        1.5px 1.5px 2.5px 1.5px rgba(0, 0, 0, 0.5);
+    }
+  }
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: 0.5s;
+  }
+  .slide-enter,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translateX(-50px);
   }
   .city {
     position: absolute;
-    z-index: 2;
+    z-index: 9;
     width: 500px;
-    bottom: calc(50% - 200px);
-    right: 17%;
+    bottom: calc(50% - 150px);
+    right: 15%;
     img {
       width: 100%;
     }
   }
   .rider {
     position: absolute;
-    z-index: 3;
+    z-index: 10;
     width: 500px;
     bottom: calc(50% - 280px);
     right: 5%;
