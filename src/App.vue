4facbe645d4495c0f398805f4991b05d55fa5b35
sashapoliti <sas.politi@gmail.com>
2024-07-02 11:35:36 +0200

    add api url in store and axios method in app for all restaurants

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,43 @@
 </template>
 
 <script>
-  import HeaderComponent from './components/HeaderComponent.vue';
-  export default {
-    name: 'App',
-    components: {
-      HeaderComponent
-    }
-  }
+import { store } from "./store";
+import axios from "axios";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import { onMounted } from "vue";
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getRestaurants() {
+      axios
+        .get(this.store.api.baseUrl + this.store.api.restaurants)
+        .then((response) => {
+          console.log(response.data);
+          this.store.data.restaurants = response.data;
+        })
+        .catch((error) => {})
+        .finally(() => {});
+    },
+  },
+  mounted() {
+    console.log("mounted");
+    this.getRestaurants();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import './assets/styles/partials/_variables.scss';
+@import "./assets/styles/partials/_variables.scss";
 
-  main {
-    height: 100%;
-  }
+main {
+  height: 100%;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    apiUrl: '',
-    apiKey: '',
-    apiUrlError: false
+    api: {
+        baseUrl: 'http://127.0.0.1:8000/api/',
+        restaurants: 'restaurants/',
+    },
+    data: {
+        restaurants: [],
+    }
 });
