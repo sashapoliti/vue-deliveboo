0a67d5fdfaf336c789d12162fae8a57d6b158652
sashapoliti <sas.politi@gmail.com>
2024-07-08 22:06:25 +0200

    add modal

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,25 @@
 <template>
-    <div v-if="store.restaurantcart != store.data.restaurant.slug && store.restaurantcart != '' "  class="alert alert-warning">
-      <div class="d-flex justify-content-between">
-        Puoi solo ordinare un ristorante alla volta
-        <!-- <RouterLink :to="{ name: 'restaurant', params: { slug: store.restaurantcart } }">Ritorna al ristorante</RouterLink> -->
+<!-- Modal -->
+<div class="modal fade" :class="{ show: showModal }" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Ops..!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Sembra tu abbia nel tuo carrello dei piatti del ristorante {{ store.restaurantcart.name }}. <br>
+        Torna indietro oppure svuota il carrello e continua su {{ store.data.restaurant.name }}!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
+  </div>
+</div>
+
+
   <div class="row">
     <div
       class="col-12 col-md-6 col-lg-4 mb-4"
@@ -13,29 +28,48 @@
     >
       <div class="card h-100">
         <img
-          :src="product.image ? 'http://127.0.0.1:8000/storage/' + product.image : this.store.api.defaultImg"
+          :src="
+            product.image
+              ? 'http://127.0.0.1:8000/storage/' + product.image
+              : store.api.defaultImg
+          "
           @error="setDefaultImage"
           class="card-img-top"
           :alt="product.name"
         />
-        <div class="card-body">
+        <div class="card-body d-flex flex-column justify-content-between">
           <h3 class="card-title">{{ product.name }}</h3>
           <div class="d-flex justify-content-between">
             <h5>{{ product.price }} €</h5>
-            <div class="changeQuantity d-flex align-items-center justify-content-center">
-              <button class="changeMinus" @click="updateQuantity(product, product.quantity - 1)" >
+            <div
+              class="changeQuantity d-flex align-items-center justify-content-center"
+            >
+              <button
+                class="changeMinus"
+                @click="updateQuantity(product, product.quantity - 1)"
+              >
                 -
               </button>
               <span class="mx-2">{{ product.quantity }}</span>
-              <button class="changePlus" @click="updateQuantity(product, product.quantity + 1)" >
+              <button
+                class="changePlus"
+                @click="updateQuantity(product, product.quantity + 1)"
+              >
                 +
               </button>
             </div>
           </div>
           <div class="d-flex justify-content-center">
             <button
-              class="btn btn-danger red-button " :class="{ disabled: store.restaurantcart != store.data.restaurant.slug && store.restaurantcart != '' }"
-              @click="store.functions.addToCart(product)" >
+              class="btn btn-danger red-button"
+              :class="{
+                disabled:
+                  store.restaurantcart &&
+                  store.restaurantcart.id !== store.data.restaurant.id &&
+                  store.restaurantcart.id !== '',
+              }"
+              @click="store.functions.addToCart(product)"
+            >
               Aggiungi al carrello
             </button>
           </div>
@@ -56,9 +90,20 @@ export default {
   data() {
     return {
       store,
+      showModal: false
     };
   },
+  mounted() {
+    this.checkRestaurantCart();
+  },
   methods: {
+    checkRestaurantCart() {
+      if (store.restaurantcart && store.restaurantcart.id !== store.data.restaurant.id && store.restaurantcart.id !== '') {
+        this.showModal = true;
+      } else {
+        this.showModal = false;
+      }
+    },
     updateQuantity(product, quantity) {
       if (quantity < 1) {
         quantity = 1;
@@ -76,13 +121,23 @@ export default {
         : store.api.defaultImg;
     },
     sortedPlates() {
-      return this.store.data.restaurant.plates.slice().sort((a, b) => a.name.localeCompare(b.name));
-    }
+      return this.store.data.restaurant.plates
+        .slice()
+        .sort((a, b) => a.name.localeCompare(b.name));
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.modal {
+  display: none;
+}
+.modal.show {
+  display: block;
+  background: rgba(0, 0, 0, 0.5);
+}
+
 .red-button {
   text-transform: uppercase;
   margin: 10px 0px 5px 0px;
--- src/store.js
+++ src/store.js
@@ -1,73 +1,86 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 export const store = reactive({
-    api: {
-        baseUrl: 'http://127.0.0.1:8000/api/',
-        imgBasePath: 'http://127.0.0.1:8000/storage/',
-        defaultImg: '/images/placeholder.svg',
-        restaurants: 'restaurants/',
-        types: 'types/',
+  api: {
+    baseUrl: "http://127.0.0.1:8000/api/",
+    imgBasePath: "http://127.0.0.1:8000/storage/",
+    defaultImg: "/images/placeholder.svg",
+    restaurants: "restaurants/",
+    types: "types/",
+  },
+  data: {
+    types: [],
+    restaurant: {},
+    totalPrice: 0,
+    emptyCart: "",
+  },
+  cart: [],
+  restaurantcart: null,
+  flag: false,
+  functions: {
+    addToCart(product) {
+      if (!store.restaurantcart) {
+        store.restaurantcart = {
+          id: store.data.restaurant.id,
+          name: store.data.restaurant.name,
+          slug: store.data.restaurant.slug,
+        };
+        console.log(store.restaurantcart.id);
+      }
+      const cartItem = store.cart.find(
+        (item) => item.product.id === product.id
+      );
+      if (cartItem) {
+        cartItem.quantity += product.quantity;
+      } else {
+        store.cart.push({
+          product: { ...product },
+          quantity: product.quantity,
+        });
+      }
+      this.saveCart();
     },
-    data: {
-        types: [],
-        restaurant: {},
-        totalPrice: 0,
-        emptyCart: ''
+    removeFromCart(product) {
+      const index = store.cart.findIndex(
+        (item) => item.product.id === product.id
+      );
+      if (index !== -1) {
+        store.cart.splice(index, 1);
+      }
+      if (store.cart.length === 0) {
+        store.restaurantcart = null;
+        console.log(store.restaurantcart);
+      }
+      this.saveCart();
     },
-    cart: [],
-    restaurantcart:'',
-    flag: false,
-    functions:{
-        addToCart(product) {
-            if(store.restaurantcart == ''){
-               store.restaurantcart = store.data.restaurant.slug;
-               console.log(store.restaurantcart);
-            }
-            const cartItem = store.cart.find(item => item.product.id === product.id);
-            if (cartItem) {
-              cartItem.quantity += product.quantity;
-            } else {
-              store.cart.push({ product: { ...product }, quantity: product.quantity });
-            }
-            this.saveCart();
-          },
-          removeFromCart(product) {
-            const index = store.cart.findIndex(item => item.product.id === product.id);
-            if (index !== -1) {
-              store.cart.splice(index, 1);
-            }
-            if(store.cart.length == 0){
-              store.restaurantcart = '';
-              console.log(store.restaurantcart);
-            }
-            this.saveCart();
-          },
-          saveCart() {
-            localStorage.setItem('cart', JSON.stringify(store.cart));
-            localStorage.setItem('restaurantcart', store.restaurantcart); 
-          },
-          loadCart() {
-            const cart = localStorage.getItem('cart');
-            if (cart) {
-              store.cart = JSON.parse(cart);
-            }
-            const restaurantcart = localStorage.getItem('restaurantcart');
-            if (restaurantcart) {
-              store.restaurantcart = restaurantcart;
-            }
-          },
-          updateQuantity: (product, quantity) => {
-            const cartItem = store.cart.find(item => item.product.id === product.id);
-            if (cartItem && quantity > 0) {
-              cartItem.quantity = quantity;
-            } else if (cartItem && quantity <= 0) {
-              store.functions.removeFromCart(product);
-            }
-            if(store.cart.length == 0){
-              store.restaurantcart = '';
-              console.log(store.restaurantcart);
-            }
-            store.functions.saveCart();
-          }
+    saveCart() {
+      localStorage.setItem("cart", JSON.stringify(store.cart));
+      localStorage.setItem('restaurantcart', JSON.stringify(store.restaurantcart));
+    },
+    loadCart() {
+      const cart = localStorage.getItem("cart");
+      if (cart) {
+        store.cart = JSON.parse(cart);
+      }
+      const restaurantcart = localStorage.getItem("restaurantcart");
+      if (restaurantcart) {
+        store.restaurantcart = JSON.parse(restaurantcart);
     }
+    },
+    updateQuantity: (product, quantity) => {
+      const cartItem = store.cart.find(
+        (item) => item.product.id === product.id
+      );
+      if (cartItem && quantity > 0) {
+        cartItem.quantity = quantity;
+      } else if (cartItem && quantity <= 0) {
+        store.functions.removeFromCart(product);
+      }
+      if (store.cart.length === 0) {
+        store.restaurantcart = null;
+        console.log(store.restaurantcart.id);
+    }
+      store.functions.saveCart();
+    },
+  },
 });
